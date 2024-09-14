de947790d5290f75cb4e9977fdeb4acfa9956512
DavPra <david.praschak@outlook.de>
2024-09-14 12:48:08 +0200

    command and main updated

--- src/main/java/application_at_upstream_mobility/bored/BoredApplication.java
+++ src/main/java/application_at_upstream_mobility/bored/BoredApplication.java
@@ -3,11 +3,10 @@ package application_at_upstream_mobility.bored;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "application_at_upstream_mobility.bored")
 public class BoredApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BoredApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BoredApplication.class, args);
+    }
 }
+
---
+++ src/main/java/application_at_upstream_mobility/bored/commands/ActivityCommand.java
@@ -0,0 +1,23 @@
+package application_at_upstream_mobility.bored.commands;
+
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import application_at_upstream_mobility.bored.controller.ActivityController;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ShellComponent
+public class ActivityCommand {
+
+    private final ActivityController activityController;
+
+    @Autowired
+    public ActivityCommand(ActivityController activityController) {
+        this.activityController = activityController;
+    }
+
+    @ShellMethod(key = "activity", value = "Get a random activity")
+    public String activity() {
+        return activityController.getActivity().getBody("activity");
+    }
+}
