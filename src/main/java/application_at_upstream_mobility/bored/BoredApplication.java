96864305641a04dcbc4bd0ae069a573ce56e0b99
DavPra <david.praschak@outlook.de>
2024-09-14 08:19:15 +0200

    Boilerplate updated

---
+++ src/main/java/application_at_upstream_mobility/bored/BoredApplication.java
@@ -0,0 +1,13 @@
+package application_at_upstream_mobility.bored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/application_at_upstream_mobility/bored/BoredApplicationTests.java
@@ -0,0 +1,13 @@
+package application_at_upstream_mobility.bored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
